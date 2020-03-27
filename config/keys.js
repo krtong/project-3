b0c4274aec927e956433ba6c72a8842cb1539458
krtong <kevin.r.tong@gmail.com>
2020-03-26 20:48:18 -0700

    first commit

---
+++ config/keys.js
@@ -0,0 +1,16 @@
+module.exports = {
+    google: {
+      clientID: '673895943430-9hda66bfcjdovm2o8jqkiget21jfor9i.apps.googleusercontent.com',
+      clientSecret: 'm17zFLlOyGNd3mvn0p2PeAQT'
+    },
+    facebook: {
+      clientID: '241784767192373',
+      clientSecret: 'b7f4c1a3fc3bd8d423a4342072380d25'
+    },
+    mongodb: {
+      dbURI: 'mongodb+srv://codybrock:802Succeed!@cluster0-k2qjf.mongodb.net/test?retryWrites=true&w=majority'
+    },
+    session: {
+      cookieKey: 'ilikebananasandmonkeys'
+    }
+  }
