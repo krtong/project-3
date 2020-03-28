f853627e0206573b4608b47dccde7d9f26d40a76
Christopher McKee <chris.mckee.13@gmail.com>
2020-03-27 18:49:45 -0600

    saves new recipie selections to DB

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -34,7 +34,7 @@ module.exports = {
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.User.findOneAndUpdate({ _id: req.params.id }, req.body)
+    db.User.findOneAndUpdate({ userID: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
