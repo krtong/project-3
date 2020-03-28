0f48a3ee70c0bb6a9495a34561c82705b6eda733
eternal golden bread <codylewisbrock@gmail.com>
2020-03-28 08:17:37 -0700

    adds bcrypt package, adds password attribute to user model, uses bcrypt to hash user password before saving to db

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -4,6 +4,8 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const FacebookStrategy = require('passport-facebook').Strategy;
 const LocalStrategy = require('passport-local').Strategy;
 
+const bcrypt = require('bcrypt');
+
 const keys = require('./keys');
 const User = require('../models/user')
 
@@ -76,17 +78,17 @@ passport.use(
   })
 )
 
-//From the passport documentation
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    User.findOne({ username: username }, function (err, user) {
-      if (err) { return done(err); }
-      if (!user) { return done(null, false); }
-      if (!user.verifyPassword(password)) { return done(null, false); }
-      return done(null, user);
-    });
-  }
-));
+// //From the passport documentation
+// passport.use(new LocalStrategy(
+//   function(username, password, done) {
+//     User.findOne({ username: username }, function (err, user) {
+//       if (err) { return done(err); }
+//       if (!user) { return done(null, false); }
+//       if (!user.verifyPassword(password)) { return done(null, false); }
+//       return done(null, user);
+//     });
+//   }
+// ));
 
 passport.use(
   new LocalStrategy(
@@ -98,13 +100,32 @@ passport.use(
           done(null, currentUser)
         } else {
           // if not, create new user in db
-          new User({
+          const newUser = new User({
             username: username,
             password: password,
-          }).save().then((newUser) => {
-            console.log(`new user created: ${newUser}`)
-            done(null, newUser)
-          })
+          });
+
+          console.log("arrived before bcrypt");
+          console.log(newUser)
+
+          bcrypt.genSalt(10, (err, salt) => 
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+              if (err) throw err;
+              // Set password to hashed
+              newUser.password = hash;
+              // Save User
+              newUser.save()
+                .then(user => {
+                  done(null, user)
+                })
+                .catch(err => console.log(err));
+            }))
+          
+          // .save().then((newUser) => {
+          //   console.log(`new user created: ${newUser}`)
+          //   done(null, newUser)
+          // })
+
         }
       })
     }
